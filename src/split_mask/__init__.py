39bd979cc3c268905a6dab3899574a6272bdfb0d
d.mihalkin <d.mihalkin@acti.ru>
2022-11-18 10:10:29 +0300

    rm old files

--- src/split_mask/__init__.py
+++
@@ -1,2 +0,0 @@
-from .point_hole_eliminator import point_holes_eliminator
-from .split_mask import split_mask
--- src/utils/utils/polygon/peaks/__init__.py
+++
@@ -1,3 +0,0 @@
-from .v1 import get_peaks_v1
-from .v2 import get_peaks_v2
-from .v3 import get_peaks_v3, get_poly_stats, get_actual_intervals, get_intersected_intervals, get_separated_intervals
