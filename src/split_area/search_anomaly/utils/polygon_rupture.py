4a72c00f406c97371d13abec478d8b12678c2bb7
d.mihalkin <d.mihalkin@acti.ru>
2022-11-21 22:16:43 +0300

    add comments

--- src/split_area/search_anomaly/utils/polygon_rupture.py
+++ src/split_area/search_anomaly/utils/polygon_rupture.py
@@ -8,14 +8,22 @@ def find_rapture_by_axis(array, axis):
         else:
             raise NotImplemented('Incorrect array format')
 
+    # take all pixel Y coordinates
     stats = list(Counter(array[:, axis]).keys())
     stats.sort()
+
+    # ищем разницу между соседними значениями координат пикселей
     tmp_stats = []
     for i in range(len(stats) - 1):
         tmp_stats.append(abs(stats[i] - stats[i + 1]))
 
+    # максимальное значение разрыва между координатами
     max_value = max(tmp_stats)
+
+    # получение индекса минимальной координаты по Y у верхней полилинии
     max_idx = tmp_stats.index(max_value)
-    y_min_threshold = min(stats[max_idx], stats[-1])
-    y_max_threshold = min(stats[max_idx+1], stats[-1])
+
+    # получение пиковых точек верхней и нижней полилиний(выборс в сторону друг друга)
+    y_min_threshold = stats[max_idx]
+    y_max_threshold = stats[max_idx + 1]
     return y_min_threshold, y_max_threshold, max_idx
