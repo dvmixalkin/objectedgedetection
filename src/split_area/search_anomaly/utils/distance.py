0968fedbe1fe296252a44fa81093dbcdbd9698a9
d.mihalkin <d.mihalkin@acti.ru>
2022-11-21 22:13:53 +0300

    add automatic thresholding

--- src/split_area/search_anomaly/utils/distance.py
+++ src/split_area/search_anomaly/utils/distance.py
@@ -1,4 +1,6 @@
 import numpy as np
+from shapely.geometry import Polygon
+from .get_np_points_from_polygon import get_np_points_from_polygon
 
 
 def get_manhattan_distance(point1, point2):
@@ -7,14 +9,41 @@ def get_manhattan_distance(point1, point2):
     return x + y
 
 
-def filter_through_manhattan_distance(polygon, min_distance=5):
-    polygon_points = []
-    for point in polygon[0]:
-        if polygon_points:
-            distance = get_manhattan_distance(point, polygon_points[-1])
-            if distance >= min_distance:
+def filter_through_manhattan_distance(polygon, min_distance='auto'):
+    def fn(min_distance, polygon):
+        polygon_points = []
+        for point in polygon[0]:
+            if polygon_points:
+                distance = get_manhattan_distance(point, polygon_points[-1])
+                if distance >= min_distance:
+                    polygon_points.append(point)
+            else:
                 polygon_points.append(point)
-        else:
-            polygon_points.append(point)
+        return polygon_points
 
-    return np.array([polygon_points])
+    if isinstance(min_distance, str):
+        orig_poly = Polygon(polygon[0])
+        local_polygons = []
+        start_points = 5
+        for dist in range(start_points, 20):
+            local_polygons.append(Polygon(fn(dist, polygon)))
+        areas = [abs(polygon.area - orig_poly.area) for polygon in local_polygons]
+        min_difference = min(areas)
+        min_difference_element_index = areas.index(min_difference)
+        print(f'min_distance = {min_difference_element_index + start_points}')
+        the_chosen_one = local_polygons[min_difference_element_index]
+
+        return get_np_points_from_polygon(the_chosen_one)
+    elif isinstance(min_distance, int):
+        polygon_points = []
+        for point in polygon[0]:
+            if polygon_points:
+                distance = get_manhattan_distance(point, polygon_points[-1])
+                if distance >= min_distance:
+                    polygon_points.append(point)
+            else:
+                polygon_points.append(point)
+
+        return np.array([polygon_points])
+    else:
+        raise NotImplemented
