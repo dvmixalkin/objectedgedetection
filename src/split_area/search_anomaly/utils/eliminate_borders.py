54d8552156afaa860d88fd1c29d5ef9b023bb5ca
d.mihalkin <d.mihalkin@acti.ru>
2022-11-21 22:14:24 +0300

    add 'pixel_limit' variable

--- src/split_area/search_anomaly/utils/eliminate_borders.py
+++ src/split_area/search_anomaly/utils/eliminate_borders.py
@@ -1,10 +1,10 @@
 from .filters import filter_polygon_by_indices
 
 
-def eliminate_borders(polygon, border_fraction=0.15, return_data='polygon'):
+def eliminate_borders(polygon, border_fraction=0.15, return_data='polygon', pixel_limit=30):
     x, y = polygon[0, :, 0], polygon[0, :, 1]
     width = x.max() - x.min()
-    separate_threshold = min(int(width * border_fraction), 30)
+    separate_threshold = min(int(width * border_fraction), pixel_limit)
     separate_x_min = x.min() + separate_threshold
     separate_x_max = x.max() - separate_threshold
     interval = [separate_x_min, separate_x_max]
