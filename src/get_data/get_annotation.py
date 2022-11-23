0f4d1d137241eb94bf4642d6bdd6757e8c639184
dvmixalkin <dvmixalkin@gmail.com>
2022-11-23 09:21:02 +0300

    add json parser for source jsons

--- src/get_data/get_annotation.py
+++ src/get_data/get_annotation.py
@@ -3,35 +3,25 @@ import numpy as np
 
 
 def original_jsons(anno_object):
-    raise NotImplemented
-    # # START - for original jsons
-    # import cv2
-    # import random
-    # from PIL import Image
-    # rgb_np_imge = np.array(Image.fromarray(image_object).convert('RGB'))
-    # all_boxes = []
-    # im = rgb_np_imge.copy()
-    # for item in anno_object['regions']:
-    #     if item['tnved'] is None and item['label'] == 'Кузов':  # len(item['points']) > 2:
-    #         polygon = np.array(item['points'])
-    #         x_min, x_max = polygon[:, 0].min(), polygon[:, 0].max()
-    #         y_min, y_max = polygon[:, 1].min(), polygon[:, 1].max()
-    #         all_boxes.append([x_min, x_max, y_min ,y_max])
-    #         im = cv2.polylines(
-    #             img=im,
-    #             pts=[np.array(item['points'])],
-    #             isClosed=True,
-    #             color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    #             thickness=5
-    #         )
-    # Image.fromarray(im).show()
-    #
-    # np_all_boxes = np.array(all_boxes)
-    #
-    # x_min, y_min = np_all_boxes[:, 0].min(), np_all_boxes[:, 1].min()
-    # x_max, y_max = np_all_boxes[:, 0].max(), np_all_boxes[:, 1].max()
-    # coordinates = [x_min, y_min, x_max, y_max]
-    # return coordinates
+    try:
+        # priority_labeler_list = [
+        #     'КОМП', 'Александр', 'kayla', 'Сергей', 'Пользователь', 'Сергей', '2002kh', '2002k', 'user', 'asus',
+        #     'HOMEr', 'kiyh', 'Крылова Виктория', 'Home', 'банан228', 'Jokerit55', 'user', 'marw1xx',
+        #     'vladimir', 'Светлана', 'Бедолага', 'marys', 'olgal', 'artem', 'ольга', 'Ольга', 'SPECIALIST'
+        # ]
+        # test version
+        data = anno_object['regions']
+        coordinates = []
+        for obj_ in data:
+            if obj_['tnved'] is not None:  # data['user_role'],
+                points = np.array(obj_['points'])
+                x_min, y_min = points[:, 0].min(), points[:, 1].min()
+                x_max, y_max = points[:, 0].max(), points[:, 1].max()
+                coordinates.append([x_min, y_min, x_max, y_max])
+    except:
+        # yolov5 version
+        pass
+    return np.array(coordinates)
 
 
 def get_coordinates_to_crop(json_data, frmt='yolo_output'):
@@ -46,7 +36,13 @@ def get_coordinates_to_crop(json_data, frmt='yolo_output'):
         x_max, y_max = np_all_boxes[:, 2].max(), np_all_boxes[:, 3].max()
         coordinates = [x_min, y_min, x_max, y_max]
     elif frmt == 'source_data':
-        coordinates = original_jsons(json_data)
+        all_coordinates = original_jsons(json_data)
+        coordinates = [
+            all_coordinates[:, 0].min(),
+            all_coordinates[:, 1].min(),
+            all_coordinates[:, 2].max(),
+            all_coordinates[:, 3].max()
+        ]
     else:
         raise NotImplemented
     return coordinates
