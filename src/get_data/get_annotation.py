ad70fa83c06ac64b0279ba6e12d5496e46110803
d.mihalkin <d.mihalkin@acti.ru>
2022-11-25 17:51:03 +0300

    add case for all boxes and each box separately

--- src/get_data/get_annotation.py
+++ src/get_data/get_annotation.py
@@ -41,17 +41,20 @@ def has50_intersection(good_box, cargo_box):
     return mask
 
 
-def get_coordinates_to_crop(image, json_data, frmt='yolo_output'):
+def get_coordinates_to_crop(image, json_data, frmt='yolo_output', version=1):
     coordinates = []
     assert frmt in ['yolo_output', 'preprocessed', 'source_data'], 'Select correct format.'
     all_boxes = []
     if frmt == 'yolo_output':
         for box in json_data['prediction']:
             all_boxes.append(box['coord'])
-        np_all_boxes = np.array(all_boxes)
-        x_min, y_min = np_all_boxes[:, 0].min(), np_all_boxes[:, 1].min()
-        x_max, y_max = np_all_boxes[:, 2].max(), np_all_boxes[:, 3].max()
-        coordinates = [[x_min, y_min, x_max, y_max]]
+        if version == 1:
+            np_all_boxes = np.array(all_boxes)
+            x_min, y_min = np_all_boxes[:, 0].min(), np_all_boxes[:, 1].min()
+            x_max, y_max = np_all_boxes[:, 2].max(), np_all_boxes[:, 3].max()
+            coordinates = [[x_min, y_min, x_max, y_max]]
+        elif version == 2:
+            coordinates = all_boxes
     elif frmt == 'source_data':
         cargo_coordiantes, all_coordinates = original_jsons(json_data)
         per_cargo_coordinates = []
