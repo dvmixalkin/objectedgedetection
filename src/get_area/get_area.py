debd23edd5454cb3a2179acf6299070516072021
d.mihalkin <d.mihalkin@acti.ru>
2022-11-25 17:51:44 +0300

    refactor fn : manual_statistical_removing

--- src/get_area/get_area.py
+++ src/get_area/get_area.py
@@ -3,10 +3,70 @@ import numpy as np
 
 from .eliminate_holes_and_tiny_objects import eliminate_holes_and_tiny_objects, mask2poly, polygon2mask
 from .filter_with_thresholds import auto_thresholding_v1, auto_thresholding_v2, auto_thresholding_v3
+from ..split_area.search_anomaly.utils.get_np_points_from_polygon import get_np_points_from_polygon
 
 # unused temporal modules
 from PIL import Image
 from src.ideas.visualize import vis_image, vis_contours, vis_polygon
+
+
+def dirty_polygon_cleaner(dirty_mask, axis):
+    object_mask = dirty_mask > 0
+
+    # binary_filtered = thresh > thresh[object_mask].mean()
+    polygons = mask2poly(object_mask)
+
+    # get areas of all polygons
+    areas = [polygon.area for polygon in polygons]
+
+    # calculate threshold for polygon areas
+    threshold = np.quantile(np.array(areas), 0.85) if axis==1 else np.array(areas).mean()/2
+
+    # get polygon indexes with area above threshold
+    idxs = [idx for idx, area in enumerate(areas) if area >= threshold]
+
+    # get filtered polygons
+    filtered_polygons = [polygons[i] for i in idxs]
+
+    # convert back polygons to mask
+    res_mask = polygon2mask(filtered_polygons, dirty_mask.shape)
+    return res_mask
+
+
+def clean_mask_along_axis(mask, axis):
+    mask_copy = mask.copy()
+
+    # count active pixel num in every row of mask
+    full_lines_mask = (mask_copy > 0).sum(axis=axis)
+
+    # set zeros to rows with num pixels above threshold
+    if axis == 0:
+        # compute threshold for pixel num in column
+        indexes = full_lines_mask < full_lines_mask.mean() / 2
+        mask_copy[:, indexes] = 0
+    else:
+        # compute threshold for pixel num in row
+        indexes = full_lines_mask > (np.quantile(full_lines_mask, 0.975) + np.median(full_lines_mask)) / 2
+        mask_copy[indexes, :] = 0
+    # convert cleared mask to polygon
+    polygons = mask2poly(mask_copy)
+
+    # get areas of all polygons
+    areas = [polygon.area for polygon in polygons]
+
+    # calculate threshold for polygon areas
+    threshold = np.mean(np.array(areas))
+
+    # get polygon indexes with area above threshold
+    idxs = [idx for idx, area in enumerate(areas) if area > threshold]
+
+    # get filtered polygons
+    filtered_polygons = [polygons[i] for i in idxs]
+
+    # convert back polygons to mask
+    dirty_mask = polygon2mask(filtered_polygons, mask.shape)
+    cleaned_mask = dirty_polygon_cleaner(dirty_mask, axis)
+    return cleaned_mask
 
 
 def manual_statistical_removing(thresh):
@@ -33,64 +93,33 @@ def manual_statistical_removing(thresh):
     mask = polygon2mask(filtered_polygons, thresh.shape)
 
     # ============================== remove horizontal artifacts ===================================================== #
-    mask_copy = mask.copy()
+    mask = clean_mask_along_axis(mask, axis=1)
 
-    # count active pixel num in every row of mask
-    full_lines_mask = (mask_copy > 0).sum(axis=1)
+    # ============================== remove vertical artifacts ===================================================== #
+    mask = clean_mask_along_axis(mask, axis=0)
 
-    # compute threshold for pixel num in row
-    indexes = full_lines_mask > (np.quantile(full_lines_mask, 0.975) + np.median(full_lines_mask))/2
-
-    # set zeros to rows with num pixels above threshold
-    mask_copy[indexes] = 0
-
-    # convert cleared mask to polygon
-    polygons = mask2poly(mask_copy)
-
-    # get areas of all polygons
-    areas = [polygon.area for polygon in polygons]
-
-    # calculate threshold for polygon areas
-    threshold = np.median(np.array(areas))
-
-    # get polygon indexes with area above threshold
-    idxs = [idx for idx, area in enumerate(areas) if area > threshold]
-
-    # get filtered polygons
-    filtered_polygons = [polygons[i] for i in idxs]
-
-    # convert back polygons to mask
-    cleaned_mask = polygon2mask(filtered_polygons, thresh.shape)
-
-    # ================-> tries to combine cleared mask width <-=========================================================
-    object_mask = cleaned_mask > 0
-    binary_filtered = thresh > thresh[object_mask].mean()
-    polygons = mask2poly(binary_filtered)
-    # get areas of all polygons
-    areas = [polygon.area for polygon in polygons]
-
-    # calculate threshold for polygon areas
-    threshold = np.quantile(np.array(areas), 0.85)
-
-    # get polygon indexes with area above threshold
-    idxs = [idx for idx, area in enumerate(areas) if area > threshold]
-
-    # get filtered polygons
-    filtered_polygons = [polygons[i] for i in idxs]
-
-    # convert back polygons to mask
-    res_mask = polygon2mask(filtered_polygons, thresh.shape)
-
-    return res_mask
+    coordinates = np.hstack([get_np_points_from_polygon(poly) for poly in mask2poly(mask)])
+    x_min = coordinates[..., 0].min()
+    y_min = coordinates[..., 1].min()
+    x_max = coordinates[..., 0].max()
+    y_max = coordinates[..., 1].max()
+    coordinates = [x_min ,y_min, x_max, y_max]
+    return mask, coordinates
 
 
-def remove_platform(thresh, pad):
+def remove_platform(thresh, pad, original=True):
     h, w = thresh.shape[0], thresh.shape[1]
 
-    # thresh = manual_statistical_removing(thresh)
+    if not original:
+        thresh, coordinates = manual_statistical_removing(thresh)
+
+    x_min = pad if original else min(pad, coordinates[0])
+    y_min = pad if original else min(pad, coordinates[1])
+    x_max = w-pad if original else max(w-pad, coordinates[2])
+    y_max = h-pad if original else max(h-pad, coordinates[3])
 
     wo_pad = np.zeros_like(thresh)
-    wo_pad[pad:h-pad, pad:w-pad] = thresh[pad:h-pad, pad:w-pad]
+    wo_pad[y_min:y_max, x_min:x_max] = thresh[y_min:y_max, x_min:x_max]
     return wo_pad
 
 
@@ -120,8 +149,8 @@ def get_area(img_crop, area_type='mask', blur_mode='gaussian_blur', ksize=(7, 7)
 
     thresh = remove_platform(thresh, pad=pad)
 
-    # to smooth lone pixels
-    thresh = cv2.GaussianBlur(thresh.astype(np.uint8), (15, 15), 0) > 0
+    # # to smooth lone pixels
+    # thresh = cv2.GaussianBlur(thresh.astype(np.uint8), (15, 15), 0) > 0
     # remove holes from mask
     cleared_mask = eliminate_holes_and_tiny_objects(
         thresh, width, height, eps=None, return_type=area_type, store_single=False
