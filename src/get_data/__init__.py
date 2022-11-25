45cf3bb70b5ed6ff7889bc4282096b1100de4b25
d.mihalkin <d.mihalkin@acti.ru>
2022-11-25 17:49:15 +0300

    add auxilary functions

--- src/get_data/__init__.py
+++ src/get_data/__init__.py
@@ -1,2 +1,2 @@
-from .get_image import get_image, get_cropped_image
+from .get_image import get_image, get_cropped_image, eliminate_lines, equalize_this_v2
 from .get_annotation import get_annotation, get_coordinates_to_crop
