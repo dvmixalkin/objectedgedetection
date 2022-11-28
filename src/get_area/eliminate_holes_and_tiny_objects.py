7402557777047026062c248f3ac45fd3c0a06208
d.mihalkin <d.mihalkin@acti.ru>
2022-11-28 17:46:58 +0300

    add separete fns for tiny object removing and hole eliminators

--- src/get_area/eliminate_holes_and_tiny_objects.py
+++ src/get_area/eliminate_holes_and_tiny_objects.py
@@ -1,44 +1,26 @@
 import numpy as np
-import rasterio
-import shapely
-from rasterio import features
 from shapely.geometry import Polygon
+from .utils import mask2poly, poly2mask, poly2coordinate
 
 
-def mask2poly(mask):
-    all_polygons = []
-    for shape, value in features.shapes(mask.astype(np.uint8), mask=(mask > 0),
-                                        transform=rasterio.Affine(1.0, 0, 0, 0, 1.0, 0)):
-        all_polygons.append(shapely.geometry.shape(shape))
-    return all_polygons
-
-
-def polygon2mask(polygons, shape):
-    binary_mask = rasterio.features.rasterize(
-        polygons,
-        out_shape=shape
-    )
-    return (binary_mask*255).astype(np.uint8)
-
-
-def eliminate_holes_and_tiny_objects(target_mask, width, height, eps=None, store_single=True, return_type='polygon',
-                                     debug=False):
-    assert return_type in ['polygon', 'mask', 'coordinates'], 'Specify correct return type.'
-    all_polygons = mask2poly(mask=target_mask)
-    if all_polygons != []:
-        areas = [polygon.area for polygon in all_polygons]
-        if store_single:
-            all_polygons = all_polygons[areas.index(max(areas))]
-        else:
-            max_area = max(areas)
-            all_polygons = [all_polygons[idx] for idx, area in enumerate(areas) if area/max_area > 0.005]
+def convert2target_format(new_polygons, shape=None, return_type='polygon'):
+    if return_type == 'polygon':
+        return new_polygons
+    elif return_type == 'mask':
+        if shape is None:
+            raise 'Specify mask size.'
+        return poly2mask(new_polygons, shape)
     else:
-        return
+        return poly2coordinate(new_polygons)
+
+
+def eliminate_holes(polygons, shape, eps=None, return_type='polygon'):
+    assert return_type in ['polygon', 'mask', 'coordinate'], 'Specify return type.'
+    if isinstance(polygons, np.ndarray):
+        polygons = mask2poly(mask=polygons)
 
     new_polygons = []
-    for polygon in all_polygons:
-        if polygon.area < 1000:
-            continue
+    for polygon in polygons:
         list_interiors = []
         if eps is not None:
             for interior in polygon.interiors:
@@ -48,18 +30,39 @@ def eliminate_holes_and_tiny_objects(target_mask, width, height, eps=None, store
         new_polygon = Polygon(polygon.exterior.coords, holes=list_interiors)
         new_polygons.append(new_polygon)
 
-    if return_type == 'coordinates':
-        result = []
-        for new_polygon in new_polygons:
-            x, y = new_polygon.exterior.xy
-            coordinates = np.vstack([np.array(x.tolist()), np.array(y.tolist())]).transpose().astype(int)
-            result.append(coordinates)
-    elif return_type == 'polygon':
-        result = new_polygons
-    else:
-        result = (rasterio.features.rasterize(
-            new_polygons,
-            out_shape=(height, width)
-        ) * 255).astype(np.uint8)
+    return convert2target_format(new_polygons, shape, return_type)
 
-    return result
+
+def eliminate_tiny_objects(polygons, shape, min_size=1000, return_type='polygon'):
+    assert return_type in ['polygon', 'mask', 'coordinate'], 'Specify return type.'
+    if isinstance(polygons, np.ndarray):
+        polygons = mask2poly(mask=polygons)
+
+    new_polygons = []
+    for polygon in polygons:
+        if polygon.area < min_size:
+            continue
+        new_polygons.append(polygon)
+
+    return convert2target_format(new_polygons, shape, return_type)
+
+
+def eliminate_holes_and_tiny_objects(
+        target_mask, eps=None, store_single=True, return_type='polygon'):
+
+    assert return_type in ['polygon', 'mask', 'coordinates'], 'Specify correct return type.'
+    all_polygons = mask2poly(mask=target_mask)
+
+    if all_polygons != []:
+        areas = [polygon.area for polygon in all_polygons]
+        if store_single:
+            all_polygons = all_polygons[areas.index(max(areas))]
+        else:
+            max_area = max(areas)
+            all_polygons = [all_polygons[idx] for idx, area in enumerate(areas) if area / max_area > 0.005]
+    else:
+        return  # target_mask
+
+    new_polygons = eliminate_tiny_objects(all_polygons, target_mask.shape, min_size=1000)
+    new_polygons = eliminate_holes(new_polygons, target_mask.shape, eps=eps)
+    return convert2target_format(new_polygons, target_mask.shape, return_type)
